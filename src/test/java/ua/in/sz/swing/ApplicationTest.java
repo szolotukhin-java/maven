f3be5283c62cfc83aa78bf51846c81be453fa729
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-05-14 23:48:05 +0300

    MY text

---
+++ src/test/java/ua/in/sz/swing/ApplicationTest.java
@@ -0,0 +1,17 @@
+package ua.in.sz.swing;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ApplicationTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE})
+    void isOdd_ShouldReturnTrueForOddNumbers(int number) {
+        System.out.println("Hello " + number);
+    }
+}
