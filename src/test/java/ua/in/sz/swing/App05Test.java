2550b6630dc041799da5a7d513fe592824a5e51c
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-12-02 19:19:07 +0200

    MY text

--- src/test/java/ua/in/sz/swing/App05Test.java
+++ src/test/java/ua/in/sz/swing/App05Test.java
@@ -4,12 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-class App04Test {
-//    @Test
-//    void test_01() {
-//        log.info("property my.forkNumber: [{}]", System.getProperty("my.forkNumber"));
-//    }
-
+class App05Test {
     @Test
     void test_01() {
         log.info("test 03 01");
