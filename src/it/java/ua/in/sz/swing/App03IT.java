fdad3556ffcedb69fe4e3c335f3a15c8133d37e0
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-12-08 19:19:52 +0200

    MY text

---
+++ src/it/java/ua/in/sz/swing/App03IT.java
@@ -0,0 +1,32 @@
+package ua.in.sz.swing;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Slf4j
+class App03IT {
+    @Test
+    @SneakyThrows
+    void test_01() {
+        TimeUnit.SECONDS.sleep(1L);
+        log.info("test 03 01");
+    }
+
+    @Test
+    @SneakyThrows
+    void test_02() {
+        TimeUnit.SECONDS.sleep(1L);
+        log.info("test 03 02");
+    }
+
+    @Test
+    @SneakyThrows
+    void test_03() {
+        TimeUnit.SECONDS.sleep(1L);
+        log.info("test 03 03");
+    }
+}
